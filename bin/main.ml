149d7a40b3d04eb25790d50c207f73201e437c62
Bretton <qsctr@users.noreply.github.com>
2022-10-06 16:50:26 -0700

    Add command line help

--- bin/main.ml
+++ bin/main.ml
@@ -10,11 +10,23 @@ extra_vars examples debug =
 
 let () =
   let logical_dir =
-    Arg.(required & opt (some string) None & info ["logical-dir"]) in
+    let open Arg in
+    required & opt (some string) None
+      & info ~doc:"The logical directory of the Coq module" ~docv:"DIRPATH"
+        ["logical-dir"] in
   let physical_dir =
-    Arg.(required & opt (some dir) None & info ["physical-dir"]) in
-  let module_name = Arg.(required & opt (some string) None & info ["module"]) in
-  let hole_type = Arg.(required & opt (some string) None & info ["type"]) in
+    let open Arg in
+    required & opt (some dir) None
+      & info ~doc:"The physical directory of the Coq module" ~docv:"DIR"
+        ["physical-dir"] in
+  let module_name =
+    let open Arg in
+    required & opt (some string) None
+      & info ~doc:"The name of the Coq module" ~docv:"MOD" ["module"] in
+  let hole_type =
+    let open Arg in
+    required & opt (some string) None
+      & info ~doc:"The type of the terms to synthesize" ~docv:"TYPE" ["type"] in
   let max_depth =
     let open Arg in
     let parser = parser_of_kind_of_string ~kind:"a nonnegative integer" @@
@@ -23,18 +35,31 @@ let () =
         | Some n when n >= 0 -> Some n
         | _ -> None in
     required & opt (some (conv (parser, Format.pp_print_int))) None
-      & info ["max-depth"] in
+      & info ~doc:"The maximum depth of terms to synthesize" ~docv:"N"
+        ["max-depth"] in
   let params =
     let open Arg in
-    value & opt (list (pair ~sep:':' string string)) [] & info ["params"] in
-  let extra_vars = Arg.(value & opt (list string) [] & info ["extra-vars"]) in
+    value & opt (list (pair ~sep:':' string string)) []
+      & info ~doc:"The parameters to the synthesized terms"
+        ~docv:"PARAM1:TYPE1,PARAM2:TYPE2,..." ["params"] in
+  let extra_vars =
+    let open Arg in
+    value & opt (list string) []
+      & info
+        ~doc:"Extra variables (already defined) to include in the \
+          synthesized terms"
+        ~docv:"VAR1,VAR2,..." ["extra-vars"] in
   let examples =
     let open Arg in
     value & opt (list ~sep:';' (pair ~sep:'=' (list string) string)) []
-      & info ["examples"] in
-  let debug = Arg.(value & flag & info ["debug"]) in
+      & info
+        ~doc:"Input-output examples that the synthesized terms must satisfy"
+        ~docv:"INPUT1A,INPUT1B,...=OUTPUT1;INPUT2A,INPUT2B,...=OUTPUT2;..."
+        ["examples"] in
+  let debug =
+    Arg.(value & flag & info ~doc:"Enable debug output to stderr" ["debug"]) in
   let open Term in
   exit @@ eval
     ( const synth $ logical_dir $ physical_dir $ module_name $ hole_type
         $ max_depth $ params $ extra_vars $ examples $ debug
-    , info "coq_synth" )
+    , info ~doc:"Coq synthesizer" "coq_synth" )
